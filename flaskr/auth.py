f9f0eddf568c34ff3289abd777541c5a4a250aa9
Peter Jang <peter.x.jang@gmail.com>
2023-03-09 07:35:48 -0600

    Templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -10,7 +10,7 @@ from flaskr.db import get_db
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 
-@bp.route('/register', methods=('GET', 'POST'))
+@bp.route('/register/', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
         username = request.form['username']
@@ -40,7 +40,7 @@ def register():
     return render_template('auth/register.html')
 
 
-@bp.route('/login', methods=('GET', 'POST'))
+@bp.route('/login/', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
         username = request.form['username']
@@ -78,7 +78,7 @@ def load_logged_in_user():
         ).fetchone()
 
 
-@bp.route('/logout')
+@bp.route('/logout/')
 def logout():
     session.clear()
     return redirect(url_for('index'))
